c8d3b26b136e56490e45e83c131202926c87e31d
Shivam Yadav1 <shivam.yadav1@ibm.com>
2022-01-07 21:35:51 +0530

    Validation added

--- book-service/src/main/java/com/example/bookservice/configuration/BookRouter.java
+++ book-service/src/main/java/com/example/bookservice/configuration/BookRouter.java
@@ -18,16 +18,16 @@ public class BookRouter
 	{
 		return RouterFunctions.route()
 				.nest(RequestPredicates.path(Constants.ENDPOINT_VERSION),
-					builder -> {
+					builder ->
 						builder
 						.POST(Constants.BOOK_ENDPOINT,bookHandler::addBook)
-						.GET(Constants.BOOK_ENDPOINT+"/find/bookname/{bookName}",request -> bookHandler.findByBookName(request))
-						.GET(Constants.BOOK_ENDPOINT+"/find/authorname/{authorName}",bookHandler::findByAuthorName)
-						.GET(Constants.BOOK_ENDPOINT+"/find/isbn/{isbn}",bookHandler::findByIsbn)
+						.GET(Constants.BOOK_ENDPOINT+"/book/{bookName}",request -> bookHandler.findByBookName(request))
+						.GET(Constants.BOOK_ENDPOINT+"/author/{authorName}",bookHandler::findByAuthorName)
+						.GET(Constants.BOOK_ENDPOINT+"/isbn/{isbn}",bookHandler::findByIsbn)
 						.PUT(Constants.BOOK_ENDPOINT,bookHandler::updateBook)
 						.DELETE(Constants.BOOK_ENDPOINT+"/{isbn}",bookHandler::deleteBook)
-						.build();
-					})
+						.build()
+					)
 				.build();
 	}
 }
--- book-service/src/main/java/com/example/bookservice/model/Constants.java
+++ book-service/src/main/java/com/example/bookservice/model/Constants.java
@@ -8,4 +8,5 @@ public class Constants
 	public static final String BOOK_ENDPOINT = "/books";
 	public static final String UNPROCESSALE_EXCEPTION_MESSAGE = "No book exists for: ";
 	public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error";
+	public static final String CORRELATION_ID = "X-Correlation-Id";
 }
