267a7ad40e6edcbd9a6386f3a386d4c410c72543
Shivam Yadav1 <shivam.yadav1@ibm.com>
2022-01-07 21:02:55 +0530

    Added exception handling for Internal Server Exception and general exceptions'

--- book-service/src/main/java/com/example/bookservice/handler/ExceptionHandler.java
+++ book-service/src/main/java/com/example/bookservice/handler/ExceptionHandler.java
@@ -2,7 +2,6 @@ package com.example.bookservice.handler;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.example.bookservice.exception.BadRequestException;
+import com.example.bookservice.exception.InternalServerException;
+import com.example.bookservice.exception.UnprocessableEntityException;
+import com.example.bookservice.model.Constants;
 import com.example.bookservice.model.ErrorMessage;
 import com.example.bookservice.model.ExceptionResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -41,7 +43,49 @@ public class ExceptionHandler implements ErrorWebExceptionHandler
 			return this.handleBadrequestException(serverResponse,(BadRequestException) ex);
 		if(ex instanceof DuplicateKeyException)
 			return this.handleDuplicateKeyException(serverResponse, (DuplicateKeyException) ex);
-		return null;
+		if(ex instanceof UnprocessableEntityException)
+			return this.handleUnprocessableEntityException(serverResponse,(UnprocessableEntityException) ex);
+		else
+			return this.handleInternalServerException(serverResponse,ex);
+	}
+
+	private Mono<Void> handleInternalServerException(ServerHttpResponse serverResponse, Throwable ex)
+	{
+		log.error("@Book-Service:: Internal Server Error has occurred");
+		log.debug("@Book-Service:: "+ex.getLocalizedMessage());
+		
+		ErrorMessage errorMessage = ErrorMessage.builder()
+												.message(Constants.INTERNAL_SERVER_ERROR_MESSAGE)
+												.moreInfo(ex.getLocalizedMessage())
+												.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+												.build();
+		List<ErrorMessage> messages = new ArrayList<>();
+		messages.add(errorMessage);
+		byte[] response = this.getResponseAsByte(new ExceptionResponse(messages));
+		
+		DataBuffer responseData = serverResponse.bufferFactory().wrap(response);
+		serverResponse.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return serverResponse.writeWith(Mono.just(responseData));
+	}
+
+	private Mono<Void> handleUnprocessableEntityException(ServerHttpResponse serverResponse,
+																			UnprocessableEntityException ex) 
+	{
+		log.error("@Book-Service:: Unprocessable Entity has occurred");
+		ErrorMessage message = ErrorMessage.builder()
+										.message("Unprocessable Entity Passed")
+										.moreInfo(ex.getMessage())
+										.status(HttpStatus.UNPROCESSABLE_ENTITY.value())
+										.build();
+		List<ErrorMessage> errorMessage = new ArrayList<>();
+		errorMessage.add(message);
+		byte[] response = this.getResponseAsByte(new ExceptionResponse(errorMessage));
+		
+		DataBuffer responseMessage = serverResponse.bufferFactory().wrap(response);
+		serverResponse.setStatusCode(HttpStatus.UNPROCESSABLE_ENTITY);
+		
+		return serverResponse.writeWith(Mono.just(responseMessage));
 	}
 
 	private Mono<Void> handleBadrequestException(ServerHttpResponse serverResponse, BadRequestException ex)
@@ -57,20 +101,11 @@ public class ExceptionHandler implements ErrorWebExceptionHandler
 													})
 													.collect(Collectors.toList());
 		ExceptionResponse errorResponse = new ExceptionResponse(errorMessages);
-		Optional<byte[]> response = Optional.empty();
-		try 
-		{
-			response = Optional.of(objectMapper.writeValueAsBytes(errorResponse));
-		} 
-		catch (JsonProcessingException e)
-		{
-			e.printStackTrace();
-		}
 		
-		DataBuffer responseData = response.map(serverResponse.bufferFactory()::wrap)
-									.orElse(serverResponse.bufferFactory().wrap("".getBytes()));
+		byte[] response = this.getResponseAsByte(errorResponse);
+		DataBuffer responseData = serverResponse.bufferFactory().wrap(response);
+		
 		serverResponse.setStatusCode(HttpStatus.BAD_REQUEST);
-		
 		return serverResponse.writeWith(Mono.just(responseData));
 	}
 	
@@ -84,22 +119,25 @@ public class ExceptionHandler implements ErrorWebExceptionHandler
 								.build();
 		List<ErrorMessage> errorMessages = new ArrayList<>();
 		errorMessages.add(error);
-		Optional<byte[]> response = Optional.empty();
 		
+		byte[] response = this.getResponseAsByte(new ExceptionResponse(errorMessages));
+		DataBuffer responseData = serverResponse.bufferFactory().wrap(response);
+		
+		serverResponse.setStatusCode(HttpStatus.BAD_REQUEST);
+		return serverResponse.writeWith(Mono.just(responseData));
+	}
+	
+	private byte[] getResponseAsByte(ExceptionResponse errorResponse)
+	{
 		try 
 		{
-			response = Optional.of(objectMapper.writeValueAsBytes(new ExceptionResponse(errorMessages)));
+			return objectMapper.writeValueAsBytes(errorResponse);
 		} 
 		catch (JsonProcessingException e)
 		{
-			e.printStackTrace();
+			log.error("@Book-Service:: Can not write object value as byte");
+			log.debug("@Book-Service:: "+e.getLocalizedMessage());
+			throw new InternalServerException(e.getLocalizedMessage());
 		}
-		
-		DataBuffer responseData = response.map(serverResponse.bufferFactory()::wrap)
-				.orElse(serverResponse.bufferFactory().wrap("".getBytes()));
-		
-		serverResponse.setStatusCode(HttpStatus.BAD_REQUEST);
-		
-		return serverResponse.writeWith(Mono.just(responseData));
 	}
 }
--- book-service/src/main/java/com/example/bookservice/model/Constants.java
+++ book-service/src/main/java/com/example/bookservice/model/Constants.java
@@ -7,4 +7,5 @@ public class Constants
 	public static final String ENDPOINT_VERSION = "/v1";
 	public static final String BOOK_ENDPOINT = "/books";
 	public static final String UNPROCESSALE_EXCEPTION_MESSAGE = "No book exists for: ";
+	public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error";
 }
